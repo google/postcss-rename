05492faf03f175ba27c6d5bdeb497f6abfda1ae7
Carlos Israel Ortiz García <goodwine@google.com>
2025-05-22 19:25:06 -0700

    Update dependencies for postcss-rename
    - Removed use of `yarn`
    - Removed references to `bazel`
    - Updated ci.yml to checkout@v4 and setup-node@v4 actions
    - Run lint/formatting checks during ci
    - Update jest, gts, postcss, and typescript to the latest versions
    - Update minimum node engine to 20.0.0
    - Removed references to babel
    - Fixed every lint/style issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.7.1
+
+* No user-visible changes.
+
 ## 0.6.1
 
 * Don't mangle `@keyframes` breakpoints with decimal points.
--- src/index.ts
+++ src/index.ts
@@ -18,11 +18,12 @@
 import selectorParser from 'postcss-selector-parser';
 
 import {MinimalRenamer} from './minimal-renamer';
+import postcss, {AnyNode} from 'postcss';
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 namespace plugin {
   export interface Options {
-    strategy?: 'none' | 'debug' | 'minimal' | ((string) => string);
+    strategy?: 'none' | 'debug' | 'minimal' | ((part: string) => string);
     by?: 'whole' | 'part';
     prefix?: string;
     except?: Iterable<string | RegExp>;
@@ -31,15 +32,14 @@ namespace plugin {
   }
 }
 
-// eslint-disable-next-line no-redeclare
-const plugin = ({
+function plugin({
   strategy = 'none',
   by = 'whole',
   prefix = '',
   except = [],
   ids = false,
   outputMapCallback,
-}: plugin.Options = {}) => {
+}: plugin.Options = {}): postcss.Plugin {
   const exceptSet = new Set(except);
   return {
     postcssPlugin: 'postcss-rename',
@@ -50,7 +50,7 @@ const plugin = ({
         ? {}
         : null;
 
-      let rename: (string) => string;
+      let rename: (part: string) => string;
       if (strategy === 'none') {
         rename = name => name;
       } else if (strategy === 'debug') {
@@ -69,8 +69,8 @@ const plugin = ({
       }
 
       function renameNode(
-        node: selectorParser.ClassName | selectorParser.Identifier
-      ) {
+        node: selectorParser.ClassName | selectorParser.Identifier,
+      ): void {
         if (skip(node.value)) return;
 
         if (by === 'part') {
@@ -104,20 +104,20 @@ const plugin = ({
       });
 
       return {
-        Rule(ruleNode) {
-          if (
-            ruleNode.parent.type !== 'atrule' ||
-            !ruleNode.parent.name.endsWith('keyframes')
-          ) {
+        Rule(ruleNode: postcss.Rule) {
+          const parent = ruleNode.parent as AnyNode;
+          if (parent.type !== 'atrule' || !parent.name.endsWith('keyframes')) {
+            // TODO(goodwine): Use processSync().
+            // eslint-disable-next-line @typescript-eslint/no-floating-promises
             selectorProcessor.process(ruleNode);
           }
         },
         OnceExit() {
-          if (outputMapCallback) outputMapCallback(outputMap);
+          if (outputMapCallback) outputMapCallback(outputMap ?? {});
         },
       };
     },
   };
-};
+}
 
 export = plugin;
